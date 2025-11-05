764ed05d65d141e9ca4913d523d5525d49a4446c
Bandit Bytes <banditheeler2020@gmail.com>
2025-11-05 17:28:38 -0500

    update to latest

--- src/main/java/net/pixeldreamstudios/passiveskilltreeadditions/PassiveSkillTreeAdditions.java
+++ src/main/java/net/pixeldreamstudios/passiveskilltreeadditions/PassiveSkillTreeAdditions.java
@@ -1,11 +1,12 @@
 package net.pixeldreamstudios.passiveskilltreeadditions;
 
 import com.mojang.logging.LogUtils;
-import daripher.skilltree.attribute.event.AttributeEvents;
+//import daripher.skilltree.attribute.event.AttributeEvents;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
@@ -21,11 +22,11 @@ public class PassiveSkillTreeAdditions {
         LOGGER.info("Hello from Passive Skill Tree Additions!");
 
         // Register the configuration
-        ModLoadingContext.get().registerConfig(net.minecraftforge.fml.config.ModConfig.Type.CLIENT, net.pixeldreamstudios.passiveskilltreeadditions.config.ModConfig.CLIENT_SPEC);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, net.pixeldreamstudios.passiveskilltreeadditions.config.ModConfig.CLIENT_SPEC);
 
         // Register event bus
         FMLJavaModLoadingContext.get().getModEventBus().register(this);
-        MinecraftForge.EVENT_BUS.register(new AttributeEvents());
+//        MinecraftForge.EVENT_BUS.register(new AttributeEvents());
 
         if (FMLEnvironment.dist.isClient()) {
             MinecraftForge.EVENT_BUS.register(new PassiveSkillTreeAdditionsClient());
